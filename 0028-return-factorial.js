54ab34666bf5a8352e64a371b5b351e0ca9d5c75
Jon <jonathan.mcneil.90@gmail.com>
2019-01-23 14:01:27 -0500

    challenge completed

---
+++ 0028-return-factorial.js
@@ -0,0 +1,8 @@
+// Return the Factorial
+// Create a function that takes an integer and returns the factorial of that integer. That is, the integer multiplied by all positive lower integers.
+
+function factorial(int) {
+	if (int < 0) return -1;
+	else if (int == 0) return 1;
+ 	else return (int * factorial(int - 1));
+}
