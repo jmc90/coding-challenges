de89855a99ccd3c7b178e26ac05b12f80eab9bee
Jon <jonathan.mcneil.90@gmail.com>
2019-02-03 17:28:55 -0500

    challenge complete

---
+++ 0042-format-number-with-commas.js
@@ -0,0 +1,12 @@
+// Format Number with Comma(s) Separating Thousands
+// Create a function that takes a number as an argument and returns a string formatted to separate thousands.
+// Examples
+// formatNum(1000) ➞ "1,000"
+
+// formatNum(100000) ➞ "100,000"
+
+// formatNum(20) ➞ "20"
+
+function formatNum(num) {
+	return num.toLocaleString()
+}
