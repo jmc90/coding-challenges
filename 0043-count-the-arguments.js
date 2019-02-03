f71079fefdf13a14ef53c7a507bdd0cc203cf2c2
Jon <jonathan.mcneil.90@gmail.com>
2019-02-03 17:34:21 -0500

    challenge complete

---
+++ 0043-count-the-arguments.js
@@ -0,0 +1,19 @@
+// Count the Arguments
+// Create a function that returns the number of argument it was called with.
+// Examples
+// numArgs() ➞ 0
+
+// numArgs("foo") ➞ 1
+
+// numArgs("foo", "bar") ➞ 2
+
+// numArgs(true, false) ➞ 2
+
+// numArgs({}) ➞ 1
+// Notes
+// Each key given in a last hash argument counts as one argument.
+// A block counts as one argument.
+
+function numArgs(...args) {
+	return args.length
+}
