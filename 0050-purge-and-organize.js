ac473c852caabfc25a3d56100313c30655029c9f
Jon <jonathan.mcneil.90@gmail.com>
2019-02-13 15:14:50 -0500

    challenge complete

---
+++ 0050-purge-and-organize.js
@@ -0,0 +1,15 @@
+// Purge and Organize
+// Given an array of numbers, write a function that returns an array that...
+
+// Has all duplicate elements removed.
+// Is sorted from least to greatest value.
+// Examples
+// uniqueSort([1, 2, 4, 3]) ➞ [1, 2, 3, 4]
+
+// uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) ➞ [1, 2, 3, 4]
+
+// uniqueSort([6, 7, 3, 2, 1]) ➞ [1, 2, 3, 6, 7]
+
+function uniqueSort(arr) {
+	return arr.sort((a, b) => a - b).filter((num, i) => num !== arr[i + 1])
+}
